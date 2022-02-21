6a9d5cf5003ec521874de1f78e20adcab41d9c2f
nitza <Christina>
2022-02-22 00:49:23 +0200

    adding all my python directories

---
+++ draft.py
@@ -0,0 +1,7 @@
+hours = int(input("Δώσε πλήθος ωρών: "))
+minutes = int(input("Δώσε πλήθος λεπτών: "))
+seconds = int(input("Δώσε πλήθος δευτερολέπτων: "))
+
+message = str(hours) + ":" + str(minutes) + ":" + str(seconds)
+
+print(message)
